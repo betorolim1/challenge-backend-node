bedd0fe17cf9a5d573401c731981ccc0f027ad3c
Adalberto Rolim Nogueira <adalberto.nogueira@autotrac.com.br>
2022-03-09 10:23:52 -0300

    Implementa arquitetura básica

---
+++ Backend.API/ApplicationContainer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.API
+{
+    public static class ApplicationContainer
+    {
+        public static void AddApplicationContainer(this IServiceCollection service)
+        {
+
+        }
+    }
+}
